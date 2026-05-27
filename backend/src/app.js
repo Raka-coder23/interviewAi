4623e8bdb3e3d9b98b32db15acabf17846fcaaef
Raka <rkc7379r@gmail.com>
2026-05-27 10:39:08 +0530

    fixed backend errors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,9 @@
 const express =require('express')
 const cookieParser=require('cookie-parser')
 const cors =require('cors')
+const path=require('path')
 const app= express()
-
+const _dirname=path.resolve();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
@@ -14,5 +15,10 @@ const interviewRouter = require('./routes/interview.routes')
 
 app.use('/api/auth',authRouter)
 app.use('/api/interview',interviewRouter)
+app.use(express.static(path.join(__dirname, "..", "..", "frontend", "dist")));
+
+app.use((req, res) => {
+  res.sendFile(path.resolve(__dirname, "..", "..", "frontend", "dist", "index.html"));
+});
 
 module.exports=app
