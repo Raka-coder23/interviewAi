6c9f577bbe6cac33b2061b4ef51f4e854c7de460
Raka-coder23 <rkc7379r@gmail.com>
2026-05-27 11:08:37 +0530

    Update API base URL to production environment

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
  const api=axios.create({
-  baseURL:"http://localhost:3000"||"https://interviewai-svlt.onrender.com",
+  baseURL:"https://interviewai-svlt.onrender.com",
   withCredentials:true
  })
 export async function register({username,email,password}) {
