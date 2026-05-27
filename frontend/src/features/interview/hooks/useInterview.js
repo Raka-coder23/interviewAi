f2579ce5a47ed3453fca9eb0cdaf75e73c3fe223
Raka <rkc7379r@gmail.com>
2026-05-27 17:59:43 +0530

    fixed duplicate pdf blob creation

--- frontend/src/features/interview/hooks/useInterview.js
+++ frontend/src/features/interview/hooks/useInterview.js
@@ -82,26 +82,27 @@ export const useInterview=(interviewId = null)=>{
   return response?.interviewReports || []
 }
 
-const getResumePdf = async (interviewReportId) => {
-     console.log("enter")
-        setLoading(true)
-        let response = null
-        try {
-            response = await generateResumePdf({ interviewReportId })
-          console.log(response)
-            const url = window.URL.createObjectURL(new Blob([ response ], { type: "application/pdf" }))
-            const link = document.createElement("a")
-            link.href = url
-            link.setAttribute("download", `resume_${interviewReportId}.pdf`)
-            document.body.appendChild(link)
-            link.click()
-        }
-        catch (error) {
-            console.log(error)
-        } finally {
-            setLoading(false)
-        }
+const getResumePdf = async (
+    interviewReportId
+) => {
+
+    setLoading(true)
+
+    try {
+
+        await generateResumePdf({
+            interviewReportId
+        })
+
+    } catch (error) {
+
+        console.log(error)
+
+    } finally {
+
+        setLoading(false)
     }
+}
 
  useEffect(() => {
         if (interviewId) {
